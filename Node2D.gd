5953a215478f346354ed608582ec906f8d3dfbe1
thebestnom <shoval.arad@gmail.com>
2020-10-02 17:13:54 +0300

    event log

--- Node2D.gd
+++ Node2D.gd
@@ -1,31 +1,26 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var all_events = []
 
 func _input(event):
-	if event is InputEventMouseButton:
-		$Label.text = "clicked {clicked} double {doubleclick} factor {factor} button mask {button_mask} button index {button_index}".\
-			format({"clicked": event.pressed, "doubleclick": event.doubleclick, "factor": event.factor, "button_mask": event.get_button_mask(), "button_index": event.get_button_index()})
-		if event.pressed:
-			$Label3.text = "{button} was pressed".format({"button": event.button_index})
-			
-	if event is InputEventMouseMotion:
-		$Label2.text = "button mask {button_mask}".\
-			format({"button_mask": event.get_button_mask()})
-	if event is InputEventKey:
-		$Label4.text = "keyboard click {key} {clicked} shift: {shift} alt: {alt} ctrl: {ctrl}".\
-			format({"clicked": event.pressed, "key": event.keycode, \
-				"shift": event.shift, "alt": event.alt, "ctrl": event.control})
+	$Label.text = ""
+	all_events.append(event)
+	if all_events.size() > 10:
+		all_events.pop_front()
+	var text = "";
+	var e = null;
+	var i = 0;
+	while i < all_events.size():
+		e = all_events[i]
+		if e is InputEventMouseButton:
+			text = "clicked {clicked} double {doubleclick} factor {factor} button mask {button_mask} button index {button_index}".\
+				format({"clicked": e.pressed, "doubleclick": e.doubleclick, "factor": e.factor, "button_mask": e.get_button_mask(), "button_index": e.get_button_index()})
+		if e is InputEventMouseMotion:
+			text = "button mask {button_mask}".\
+				format({"button_mask": e.get_button_mask()})
+		if e is InputEventKey:
+			text = "keyboard click {key} {clicked} shift: {shift} alt: {alt} ctrl: {ctrl}".\
+				format({"clicked": e.pressed, "key": e.keycode, \
+					"shift": e.shift, "alt": e.alt, "ctrl": e.control})
+		$Label.text += "\n %s" % text
+		i += 1
